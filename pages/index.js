7a5ee13265d0d41c1b9945790d847686a6709600
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2023-08-24 01:24:38 +0530

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,13 @@
+//next image
+import {} from "next/image";
+
+//components
+import ParticlesContainer from '../components/ParticlesContainer';
+import ProjectsBtn from '../components/ProjectsBtn';
+import Avatar from '../components/Avatar';
+
+//framer motion
+
 const Home = () => {
   return <div>Home</div>;
 };
