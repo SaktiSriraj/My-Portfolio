2e261bd657e134a683be0df3d106c43957e20c5e
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2023-08-25 17:55:38 +0530

    Avatar Updated

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,8 +1,19 @@
 //next image
+import Image from "next/image";
 
 
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className='hidden xl:flex xl:max-w-none'>
+      <Image
+        src={'/avatar.png'}
+        width={500}
+        height={400}
+        alt=''
+        className="translate-z-0 w-full h-full"
+      />
+    </div>
+  );
 };
 
 export default Avatar;
