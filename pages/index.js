8b4de78468fc37bd2ab880b3f5ee521961efe0a6
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2023-08-25 17:55:18 +0530

    index updated

--- pages/index.js
+++ pages/index.js
@@ -60,9 +60,21 @@ const Home = () => {
       <div className='bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0'></div>
 
       {/* particles */}
-      <div>Particles</div>
+      <ParticlesContainer />
+
 
       {/* avatar */}
+      <motion.div
+        variants={fadeIn('up',0.2)}
+        initial='hidden'
+        animate='show'
+        exit='hidden'
+        transition={{duration: 1,ease:'easeInOut'}}
+        className='w-full h-full max-w-[500px] max-h-[400px] absolute bottom-0 lg:top-[40%] lg:right-[8%]'
+      >
+        <Avatar />
+      </motion.div>
+      
     </div>
   </div>;
 };
