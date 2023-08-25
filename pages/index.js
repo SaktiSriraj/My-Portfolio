398b8f726daa61a07749d395f1d54047fa782da4
Sakti Sriraj Mishra <reply.saktisriraj@gmail.com>
2023-08-25 11:47:40 +0530

    Updated index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,50 @@ import {motion} from 'framer-motion';
 import {fadeIn} from '../variants';
 
 const Home = () => {
-  return <div className='bg-primary/60 h-full'>Home</div>;
+  return <div className='bg-primary/60 h-full'>
+    {/* text */}
+    <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10'>
+      <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto'>
+        {/* title */}
+        <motion.h1
+          variants={fadeIn('down',0.2)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className='h1'
+        >
+          Transforming Ideas<br/> Into{' '}
+          <span className="text-accent">Digital Reality</span>
+        </motion.h1>
+        {/* subtitle */}
+        <motion.p
+          variants={fadeIn('down',0.3)}
+          initial='hidden'
+          animate='show'
+          exit='hidden'
+          className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin congue, lacus sed auctor posuere, nibh erat vestibulum urna, ut ornare nisi elit id ante. Nunc ultricies lobortis feugiat.
+          </motion.p>
+          {/* btn */}
+          <div className='flex justify-center xl:hidden relative '>
+            <ProjectsBtn/>
+          </div>
+          <motion.div
+            variants={fadeIn('down',0.4)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+            className='hidden xl:flex'
+          >
+            <ProjectsBtn />
+          </motion.div>
+      </div>
+    </div>
+
+    {/* image */}
+    <div>image</div>
+  </div>;
 };
 
 export default Home;
